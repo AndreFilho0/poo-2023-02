9b0e206ed2a429752e181d0797afccf75bfcd563
AndreFilho0 <filhoandre534@gmail.com>
2023-11-22 19:23:52 -0300

    ...a

---
+++ POO/src/teste/SwingBuscaAquivo.java
@@ -0,0 +1,36 @@
+package teste;
+
+import javax.swing.*;
+import java.io.*;
+
+public class SwingBuscaAquivo {
+    public static void main(String[] args) {
+        /* Cria objeto para utilizar a escolha de arquivos */
+        JFileChooser fc = new JFileChooser();
+        /* Mostra a janela de dialogo para escolha do arquivo */
+        int result;
+        result = fc.showOpenDialog(null);
+        /* Verifica se foi selecionada a opção Cancela da janela */
+        if (result == JFileChooser.CANCEL_OPTION)
+            System.exit(0);
+        else {
+            /* Cria objeto com referência para o arquivo físico selecionado */
+            File farqEnt = fc.getSelectedFile();
+            try {
+                /* Abre o arquivo para Leitura */
+                FileReader f = new FileReader(farqEnt);
+                /* Classe BufferedReader permite a leitura linha a linha */
+                BufferedReader in = new BufferedReader(f);
+                /* Lê linha a linha e mostra na console */
+                String linha = in.readLine();
+                while(linha !=null ){
+                    System.out.println(linha);
+                    linha = in.readLine();
+                }
+                in.close();
+            } catch (Exception e) {
+                System.out.println(e); }
+        }
+    }
+
+}
